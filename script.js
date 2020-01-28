460f25dea8b2768462ad59a9f75f132cd33839ec
cpercy83 <clinton.percy@gmail.comgit>
2020-01-28 17:58:13 -0600

    my first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  var button = document.querySelector('button');
+
+  button.addEventListener('click', function() {
+    alert("Let's Rock!!");
+  });
+  button.addEventListener('click', function() {
+    alert("Let's Really Really Rock!!");
+  });
+}
